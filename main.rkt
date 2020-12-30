709ed12dea54f641bf52072cb36872c06a5759c3
AutumnToe <just.jacob@icloud.com>
2020-12-30 10:49:03 -0700

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -11,8 +11,19 @@
 (define (draw s)
   (place-image redball 150 (state-y s) background))
 
+(define (new-y s)
+  (+ (state-y s) (state-velocity s)))
+
+(define (new-velocity s)
+  (+ (state-velocity s) 1.5))
+
+(define (bounce s)
+  (* (state-velocity s) -.5))
+  
 (define (tick s)
- (make-state (+ (state-y s) (state-velocity s)) (+ (state-velocity s) .5)))
+  (cond [(and (>= (abs (state-velocity s)) 2) (>= (state-y s) 600))
+         (make-state 599 (bounce s))]
+        [else (make-state (new-y s) (new-velocity s))]))
 
 (define (main s)
   (big-bang s
